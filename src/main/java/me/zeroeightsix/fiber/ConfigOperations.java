f8d40ad1985f882d4263d6c20b49387b47bc469a
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-03-25 22:03:41 +0100

    Add ConfigOperations

---
+++ src/main/java/me/zeroeightsix/fiber/ConfigOperations.java
@@ -0,0 +1,27 @@
+package me.zeroeightsix.fiber;
+
+import me.zeroeightsix.fiber.ir.ConfigNode;
+import me.zeroeightsix.fiber.ir.ConfigValue;
+
+public class ConfigOperations {
+
+    /**
+     * Merges two {@link ConfigNode} objects
+     * @param from  The ConfigNode that will be read from, but not mutated.
+     * @param to    The mutated {@link ConfigNode} that will inherit <code>from</code>'s values and nodes.
+     */
+    public static void mergeTo(ConfigNode from, ConfigNode to) {
+        from.getSettingsImmutable().forEach((name, value) -> mergeTo(value, to));
+        from.getSubSettingsImmutable().forEach((name, node) -> mergeTo(node, to.sub(name)));
+    }
+
+    /**
+     * Merges a leaf node ({@link ConfigValue}) into a {@link ConfigNode}
+     * @param value The leaf node to be inherited
+     * @param to    The mutated {@link ConfigNode} that will inherit <code>value</code>
+     */
+    public static void mergeTo(ConfigValue value, ConfigNode to) {
+        to.set(value.getName(), value.getValue());
+    }
+
+}
