f5d4b5364684ed33c56b08f745d4e59d1f7efe78
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-03-30 12:26:25 +0100

    Fix interface retention policies

--- src/main/java/me/zeroeightsix/fiber/annotations/Setting.java
+++ src/main/java/me/zeroeightsix/fiber/annotations/Setting.java
@@ -6,10 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Setting {
 
+    @Retention(RetentionPolicy.RUNTIME)
     @interface Final {}
 
+    @Retention(RetentionPolicy.RUNTIME)
     @interface NoForceFinal {}
 
+    @Retention(RetentionPolicy.RUNTIME)
     @interface Ignored {}
 
     Constraint[] constraints() default {};
