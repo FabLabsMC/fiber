be110f5129587894dacdecc8a8bc4632eb0cd965
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-04-03 22:14:13 +0200

    Refactor UnderscoredLowerCaseConventionTest

--- src/test/java/me/zeroeightsix/fiber/annotations/conventions/UnderscoredLowerCaseConventionTest.java
+++ src/test/java/me/zeroeightsix/fiber/annotations/conventions/UnderscoredLowerCaseConventionTest.java
@@ -1,19 +1,29 @@
 package me.zeroeightsix.fiber.annotations.conventions;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UnderscoredLowerCaseConventionTest extends UnderscoredLowerCaseConvention {
 
+    private static final String[] STRINGS = {
+            "Hello world",
+            "helloWorld",
+            "HelloWorld",
+            "Hello World",
+            "Hello_World",
+            "hello_world"
+    };
+
+    private static final String EXPECTED = "hello_world";
+
     @Test
+    @DisplayName("Name conversions")
     void testName() {
-        assertEquals("hello_world", name("Hello world"), "Hello world");
-        assertEquals("hello_world", name("helloWorld"), "helloWorld");
-        assertEquals("hello_world", name("HelloWorld"), "HelloWorld");
-        assertEquals("hello_world", name("Hello World"), "Hello World");
-        assertEquals("hello_world", name("Hello_World"), "Hello_World");
-        assertEquals("hello_world", name("hello_world"), "hello_world");
+        for (String s : STRINGS) {
+            assertEquals(EXPECTED, name(s), "Converting " + s + " to " + EXPECTED);
+        }
     }
 
 }
