3d492eb6b2e29e87e9b8ce16d93ab34c87f083b2
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-03-19 16:29:18 +0100

    Added javadocs to the tree interfaces

--- src/main/java/me/zeroeightsix/fiber/tree/Property.java
+++ src/main/java/me/zeroeightsix/fiber/tree/Property.java
@@ -1,10 +1,20 @@
 package me.zeroeightsix.fiber.tree;
 
+/**
+ * Implementing this interface means that this class has a nullable value which can be mutated using the {@link Property#setValue(Object) setValue} method.
+ * @param <T> the type of value this property holds
+ *
+ * @see Property#setValue(Object)
+ */
 public interface Property<T> extends HasValue<T> {
 
     /**
-     * @param value
-     * @return <code>true</code> if successful
+     * Sets the value of this property.
+     * <br>
+     * This can fail and return {@code false} if, for example, the value did not satisfy the constraints of the property.
+     *
+     * @param value the new value this property should receive
+     * @return {@code true} if this property changed as a result of the call
      */
     boolean setValue(T value);
 
