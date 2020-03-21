ddc782743146ec30ca4c51e3e060720dfe915c8e
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-03-21 13:22:02 +0100

    Added javadocs to ConstraintsBuilder

--- src/main/java/me/zeroeightsix/fiber/builder/constraint/ConstraintsBuilder.java
+++ src/main/java/me/zeroeightsix/fiber/builder/constraint/ConstraintsBuilder.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 import java.util.List;
 
 /**
+ * A builder for {@code Constraint}s.
+ * <br> This is the abstract base class for all builders of this type. Builders implementing this class include {@link Scalar} and {@link Aggregate}.
  *
  * @param <A> the type of {@link Constraint} this builder should output
  * @param <S> the type of this builder's source object (eg. {@code ConfigValueBuilder} or {@code ConstraintsBuilder}
@@ -14,10 +16,34 @@ import java.util.List;
  * @param <B> the type of {@code this}, for chaining
  */
 public abstract class ConstraintsBuilder<S, A, T, B extends ConstraintsBuilder<S, A, T, B>> extends AbstractConstraintsBuilder<S, A, T, B> {
+    /**
+     * Creates a new scalar constraint builder
+     *
+     * @param source the {@code ConfigValueBuilder} this {@code ConstraintsBuilder} originates from
+     * @param constraints the list of constraints this builder will add to
+     * @param type the class of the type of values checked by constraints built by this builder
+     * @param <S> the type of {@code source}
+     * @param <T> the type {@code type} represents
+     * @return the newly created builder
+     * @see Scalar
+     */
     public static <S, T> Scalar<S, T> scalar(S source, List<Constraint<? super T>> constraints, Class<T> type) {
         return new Scalar<>(source, constraints, type);
     }
 
+    /**
+     * Creates a new aggregate constraint builder
+     *
+     * @param source the {@code ConfigValueBuilder} this {@code ConstraintsBuilder} originates from
+     * @param constraints the list of constraints this builder will add to
+     * @param aggregateType the type of collection or array {@code source} holds
+     * @param componentType the type of all elements in {@code aggregateType}
+     * @param <S> the type of {@code source}
+     * @param <A> the type of {@code aggregateType}
+     * @param <T> the type of {@code componentType}
+     * @return the newly created builder
+     * @see Aggregate
+     */
     public static <S, A, T> Aggregate<S, A, T> aggregate(S source, List<Constraint<? super A>> constraints, Class<A> aggregateType, Class<T> componentType) {
         return new Aggregate<>(source, constraints, aggregateType, componentType);
     }
@@ -28,9 +54,18 @@ public abstract class ConstraintsBuilder<S, A, T, B extends ConstraintsBuilder<S
 
     public abstract CompositeConstraintBuilder<B, A> composite(CompositeType type);
 
+    /**
+     * Finishes building constraints.
+     * <br> As a result of this call, the built constraints will be added to the source {@code ConfigValue} builder.
+     *
+     * @return the source builder
+     */
     public abstract S finish();
 
     /**
+     * An implementation of {@code ConstraintsBuilder} for scalar constraints.
+     * <br>Scalar types are those with only one value, such as {@code Integer} or {@code String}.
+     * <br>The other, aggregate types, such as {@code List}s or arrays, are created using {@link Aggregate}
      *
      * @param <S> the type of this builder's source object (eg. {@code ConfigValueBuilder} or {@code ConstraintsBuilder}
      * @param <T> the type of {@link Constraint} this builder should output
@@ -53,6 +88,15 @@ public abstract class ConstraintsBuilder<S, A, T, B extends ConstraintsBuilder<S
         }
     }
 
+    /**
+     * An implementation of {@code ConstraintsBuilder} for aggregate constraints.
+     * <br>Aggregate types are those that hold multiple values, such as {@code List} or arrays.
+     * <br>The other, scalar types, such as {@code Integer} or {@code String}, are created using {@link Scalar}
+     *
+     * @param <S> the type of this builder's source object (eg. {@code ConfigValueBuilder} or {@code ConstraintsBuilder}
+     * @param <A> the type of collection or array {@code source} holds
+     * @param <T> the type of {@link Constraint} this builder should output
+     */
     public static class Aggregate<S, A, T> extends ConstraintsBuilder<S, A, A, Aggregate<S, A, T>> {
         private final Class<T> componentType;
 
