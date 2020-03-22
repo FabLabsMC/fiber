21b6980c086aed8c13707a3d52e94e968f564536
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-03-22 11:50:04 +0100

    Add javadocs to LowercaseConvention

--- src/main/java/me/zeroeightsix/fiber/annotation/convention/LowercaseConvention.java
+++ src/main/java/me/zeroeightsix/fiber/annotation/convention/LowercaseConvention.java
@@ -1,5 +1,8 @@
 package me.zeroeightsix.fiber.annotation.convention;
 
+/**
+ * A naming convention that converts any input name to lower case.
+ */
 public class LowercaseConvention implements SettingNamingConvention {
 
     @Override
