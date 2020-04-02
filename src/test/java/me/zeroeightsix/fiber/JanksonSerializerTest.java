15da89730ecd4c2e5a1bb5ff87d2d1abd357fc62
Pyrofab <redstoneinfire@gmail.com>
2020-04-02 18:07:37 +0200

    Add a test for constraint checking in deserialization

--- src/test/java/me/zeroeightsix/fiber/JanksonSerializerTest.java
+++ src/test/java/me/zeroeightsix/fiber/JanksonSerializerTest.java
@@ -1,15 +1,21 @@
 package me.zeroeightsix.fiber;
 
 import me.zeroeightsix.fiber.builder.ConfigNodeBuilder;
+import me.zeroeightsix.fiber.constraint.CompositeType;
 import me.zeroeightsix.fiber.exception.FiberException;
 import me.zeroeightsix.fiber.serialization.JanksonSerializer;
 import me.zeroeightsix.fiber.tree.ConfigNode;
+import me.zeroeightsix.fiber.tree.ConfigValue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class JanksonSerializerTest {
 
@@ -42,21 +48,78 @@ class JanksonSerializerTest {
         JanksonSerializer jk = new JanksonSerializer();
         ConfigNode nodeOne = new ConfigNodeBuilder()
                 .fork("child")
-                .beginValue("A", 10)
-                .finishValue()
+                    .beginValue("A", 10)
+                    .finishValue()
                 .finishNode()
                 .build();
 
         ConfigNodeBuilder builderTwo = new ConfigNodeBuilder();
-        ConfigNode childTwo = builderTwo.fork("child")
-                .beginValue("A", 20)
-                .finishValue()
+        ConfigNode childTwo = builderTwo
+                .fork("child")
+                    .beginValue("A", 20)
+                    .finishValue()
                 .build();
         ConfigNode nodeTwo = builderTwo.build();
 
         jk.serialize(nodeOne, bos);
         jk.deserialize(nodeTwo, new ByteArrayInputStream(bos.toByteArray()));
         NodeOperationsTest.testNodeFor(childTwo, "A", Integer.class, 10);
+    }
+
+    @Test
+    @DisplayName("Constraints")
+    void nodeSerializationConstrains() throws IOException, FiberException {
+        AtomicReference<ConfigValue<String>> versionOne = new AtomicReference<>();
+        AtomicReference<ConfigValue<Integer>> settingOne = new AtomicReference<>();
+
+        ConfigNode nodeOne = new ConfigNodeBuilder()
+                .beginValue("version", "0.1")
+                    .withFinality()
+                .finishValue(versionOne::set)
+                .fork("child")
+                    .beginValue("A", 10)
+                    .finishValue(settingOne::set)
+                .finishNode()
+                .build();
+
+        AtomicReference<ConfigValue<String>> versionTwo = new AtomicReference<>();
+        AtomicReference<ConfigValue<Integer>> settingTwo = new AtomicReference<>();
+
+        ConfigNode nodeTwo = new ConfigNodeBuilder()
+                .beginValue("version", "1.0.0")
+                .withFinality()
+                .beginConstraints() // technically redundant with final, but checks the default value
+                    .regex("\\d+\\.\\d+\\.\\d+")
+                .finishConstraints()
+                .finishValue(versionTwo::set)
+                .fork("child")
+                .beginValue("A", 20)
+                    .beginConstraints()
+                    .composite(CompositeType.OR)
+                        .atMost(0)
+                        .atLeast(20)
+                    .finishComposite()
+                    .finishConstraints()
+                .finishValue(settingTwo::set)
+                .finishNode()
+                .build();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        JanksonSerializer jk = new JanksonSerializer();
+
+        jk.serialize(nodeOne, bos);
+        jk.deserialize(nodeTwo, new ByteArrayInputStream(bos.toByteArray()));
+        assertEquals("1.0.0", versionTwo.get().getValue(), "RegEx and finality constraints bypassed");
+        assertEquals(20, settingTwo.get().getValue(), "Range constraint bypassed");
+
+        bos.reset();
+
+        versionOne.get().setValue("0.1.0");
+        settingOne.get().setValue(-5);
+        jk.serialize(nodeOne, bos);
+        jk.deserialize(nodeTwo, new ByteArrayInputStream(bos.toByteArray()));
+        assertEquals("1.0.0", versionTwo.get().getValue(), "Finality bypassed");
+        assertEquals(-5, settingTwo.get().getValue(), "Valid value rejected");
     }
 
     @Test
