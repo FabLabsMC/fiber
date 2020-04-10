823e24b5336ae61c1d8ad0b02592a5afe3a97b63
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-04-10 23:41:38 +0200

    Move MalformedFieldException to api

---
+++ src/main/java/me/zeroeightsix/fiber/api/exception/MalformedFieldException.java
@@ -0,0 +1,7 @@
+package me.zeroeightsix.fiber.api.exception;
+
+public class MalformedFieldException extends FiberException {
+    public MalformedFieldException(String s) {
+        super(s);
+    }
+}
