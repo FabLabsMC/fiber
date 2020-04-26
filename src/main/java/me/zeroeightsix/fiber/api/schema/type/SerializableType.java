3bc11cb016addb1795374fe38f777fd46088b7de
Pyrofab <redstoneinfire@gmail.com>
2020-04-26 23:35:35 +0200

    Document the new type representation classes

--- src/main/java/me/zeroeightsix/fiber/api/schema/type/SerializableType.java
+++ src/main/java/me/zeroeightsix/fiber/api/schema/type/SerializableType.java
@@ -1,11 +1,31 @@
 package me.zeroeightsix.fiber.api.schema.type;
 
+import me.zeroeightsix.fiber.api.schema.type.derived.ConfigType;
 import me.zeroeightsix.fiber.api.serialization.TypeSerializer;
 import me.zeroeightsix.fiber.impl.constraint.Constraint;
 
+import java.math.BigDecimal;
+
 /**
- * A configuration type, convertible to a config primitive.
- * @param <T>
+ * A data type that is convertible to a config primitive.
+ *
+ * <p> A {@code SerializableType} constrains the range of possible values
+ * through additional properties. This means that for each kind of
+ * {@code SerializableType} there may be an arbitrary amount of different
+ * sets of valid values, all being mapped to the same platform type.
+ * For example, {@link Integer} and {@link Double} are both represented
+ * as {@link DecimalSerializableType}s and stored as {@link BigDecimal}s,
+ * but with different constraints.
+ *
+ * @param <T> the actual platform type used to store the serializable data
+ * @see ConfigType
+ * @see BooleanSerializableType
+ * @see EnumSerializableType
+ * @see ListSerializableType
+ * @see MapSerializableType
+ * @see DecimalSerializableType
+ * @see RecordSerializableType
+ * @see StringSerializableType
  */
 public abstract class SerializableType<T> {
     private final Class<T> platformType;
--- src/main/java/me/zeroeightsix/fiber/api/schema/type/derived/ConfigType.java
+++ src/main/java/me/zeroeightsix/fiber/api/schema/type/derived/ConfigType.java
@@ -9,6 +9,23 @@ import java.lang.reflect.AnnotatedElement;
 import java.util.StringJoiner;
 import java.util.function.Function;
 
+/**
+ * A {@code ConfigType} contains serializable information describing
+ * a data type as well as the information necessary to convert a serialized
+ * value of that type to a friendlier runtime type.
+ *
+ * @param <R> the runtime type used by the client application
+ * @param <S> the serialized type used by config trees and serializers
+ * @param <T> the representation of the serialized type
+ * @see SerializableType
+ * @see BooleanConfigType
+ * @see EnumConfigType
+ * @see ListConfigType
+ * @see MapConfigType
+ * @see NumberConfigType
+ * @see RecordConfigType
+ * @see StringConfigType
+ */
 public abstract class ConfigType<R, S, T extends SerializableType<S>> {
     private final T serializedType;
     private final Class<R> runtimeType;
@@ -22,18 +39,40 @@ public abstract class ConfigType<R, S, T extends SerializableType<S>> {
         this.f0 = f0;
     }
 
-    public abstract  <U> ConfigType<U, S, T> derive(Class<? super U> runtimeType, Function<R, U> g, Function<U, R> g0);
+    /**
+     * Derive a {@code ConfigType} from this type object.
+     *
+     * <p> The new {@code ConfigType} will have the same serialized type (with the same constraints),
+     * but a different runtime type. Values will be converted between the two
+     * types using composed functions: {@code toSerializedType(x) = this.toSerializedType(g0(x))}
+     * and {@code toRuntimeType(y) = g(this.toRuntimeType(y))}.
+     *
+     * @param runtimeType a class object representing the runtime type of the new {@code ConfigType}
+     * @param g           a partial deserialization function
+     * @param g0          a partial serialization function
+     * @param <U>         the runtime type of the new {@code ConfigType}
+     * @return a derived {@code ConfigType} with the given {@code runtimeType}
+     */
+    public abstract <U> ConfigType<U, S, T> derive(Class<? super U> runtimeType, Function<R, U> g, Function<U, R> g0);
 
     public abstract ConfigType<R, S, T> withType(T newSpec);
 
+    /**
+     * Converts a runtime value from a client application to this {@code ConfigType}'s
+     * serialized type.
+     *
+     * @param runtimeValue the value to convert to serializable form
+     * @return a serializable equivalent of the runtime value
+     * @see SerializableType#getPlatformType()
+     */
     public S toSerializedType(R runtimeValue) {
         return this.f0.apply(runtimeValue);
     }
 
     /**
-     * Converts a serialized value to this converter's runtime type.
+     * Converts a serialized value to this {@code ConfigType}'s runtime type.
      *
-     * @param serializedValue the new value this property should receive
+     * @param serializedValue the value to convert to runtime form
      * @return a runtime equivalent of the serialized value
      * @throws FiberConversionException if the serialized value does not fit this converter's constraints
      */
