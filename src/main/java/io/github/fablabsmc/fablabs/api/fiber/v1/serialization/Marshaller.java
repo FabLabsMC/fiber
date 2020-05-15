15e5d39550fcd2d5ed13dfcfeca47ccdbe6a3e66
Thalia Nero <chrisvn00@gmail.com>
2020-05-15 00:55:38 -0400

    Remove Marshaller.

--- src/main/java/io/github/fablabsmc/fablabs/api/fiber/v1/serialization/Marshaller.java
+++
@@ -1,9 +0,0 @@
-package io.github.fablabsmc.fablabs.api.fiber.v1.serialization;
-
-import java.lang.reflect.Type;
-
-public interface Marshaller<T> {
-	T marshall(Object value);
-
-	<A> A marshallReverse(Type type, T value);
-}
