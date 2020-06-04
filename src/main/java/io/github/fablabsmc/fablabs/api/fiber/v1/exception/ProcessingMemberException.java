afaf982143b95dde5457838fa83ee0f0fb5587e9
Pyrofab <redstoneinfire@gmail.com>
2020-06-05 01:10:13 +0200

    Improve group errors from AnnotatedSettings (#69)
    * Remove final condition on groups
    
    * Add helpful error message on null groups
    
    * Fix checkstyle

--- src/main/java/io/github/fablabsmc/fablabs/api/fiber/v1/exception/ProcessingMemberException.java
+++ src/main/java/io/github/fablabsmc/fablabs/api/fiber/v1/exception/ProcessingMemberException.java
@@ -6,10 +6,19 @@ import java.lang.reflect.Member;
  * Thrown when an error occurs within annotated member processing.
  */
 public class ProcessingMemberException extends FiberException {
-	final Member member;
+	private final Member member;
+
+	public ProcessingMemberException(String message, Member member) {
+		super(message);
+		this.member = member;
+	}
 
 	public ProcessingMemberException(String message, Throwable cause, Member member) {
 		super(message, cause);
 		this.member = member;
+	}
+
+	public Member getMember() {
+		return this.member;
 	}
 }
